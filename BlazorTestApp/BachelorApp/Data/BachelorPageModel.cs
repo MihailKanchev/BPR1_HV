c8ddb23cf23108bec0856ced6655ca7c71f8bbb1
Mihail Kanchev <colty.gaming@gmail.com>
2020-12-09 12:09:13 +0100

    Added LoRa connection to Index page.
    Opened up a web socket listener loop to listen for data.

--- BlazorTestApp/BachelorApp/Data/BachelorPageModel.cs
+++ BlazorTestApp/BachelorApp/Data/BachelorPageModel.cs
@@ -1,6 +1,15 @@
-﻿using System;
+﻿using BachelorApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.WebSockets;
+using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BachelorApp
@@ -24,7 +33,58 @@ namespace BachelorApp
 
             return answer;
         }
-        
+
+        public async Task CollectSensorData()
+        {
+            Uri link = new Uri("wss://iotnet.teracom.dk/app?token=vnoTQQAAABFpb3RuZXQuY2liaWNvbS5ka45GqovHqK3PVCJypYVMsIw=");
+            var ws = new ClientWebSocket();
+            Console.WriteLine("Connecting ...");
+            ws.ConnectAsync(link, CancellationToken.None).Wait();
+
+            Payload payload = new Payload();
+            payload.cmd = "tx";
+            payload.EUI = "0004A30B0025A3D5";
+            payload.port = 2;
+            payload.data = "42";
+
+            var request = JsonSerializer.Serialize(payload);
+            byte[] bytes = Encoding.ASCII.GetBytes(request);
+            Console.WriteLine("Sending initial message ...");
+            ws.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None).Wait();
+
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    while (ws.State == WebSocketState.Open)
+                    {
+                        WebSocketReceiveResult result;
+                        do
+                        {
+                            var messageBuffer = WebSocket.CreateClientBuffer(1024, 16);
+                            Console.WriteLine("Receiving...");
+                            result = await ws.ReceiveAsync(messageBuffer, CancellationToken.None);
+                            ms.Write(messageBuffer.Array, messageBuffer.Offset, result.Count);
+                            if (result.EndOfMessage && result.MessageType == WebSocketMessageType.Text)
+                            {
+                                Console.WriteLine("Deserializing...");
+                                var msgString = Encoding.UTF8.GetString(ms.ToArray());
+                                Console.WriteLine(msgString);
+                                
+                                ms.Seek(0, SeekOrigin.Begin);
+                                ms.Position = 0;
+                            }
+                        }
+                        while (true);
+                    }
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("[WS] Tried to receive message while already reading one.");
+            }
+        }
+
         /*public async Task<String> OnGet()
         {
             var request = new HttpRequestMessage(HttpMethod.Get,$"http://127.0.0.1:5000/"); //HttpMethod.Get ...
@@ -41,6 +101,6 @@ namespace BachelorApp
                 return "";
             }
         }*/
-        
+
     }
 }
---
+++ BlazorTestApp/BachelorApp/Data/Payload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BachelorApp.Data
+{
+    public class Payload
+    {
+        public String cmd { get; set; }
+        public String EUI { get; set; }
+        public long port { get; set; }
+        public String data { get; set; }
+    }
+}
